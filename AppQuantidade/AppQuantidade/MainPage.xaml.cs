2b9556541ba2a2448a57d434f86a26a163420e72
Teemote <luteciorv@gmail.com>
2021-10-29 08:20:58 -0300

    Implementação da lógica e finalização do aplicativo.

--- AppQuantidade/AppQuantidade/MainPage.xaml.cs
+++ AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -1,18 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace AppQuantidade
 {
     public partial class MainPage : ContentPage
     {
+        private int _amount = 1;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Increase(object sender, EventArgs e)
+        {
+            _amount++;
+
+            lblAmount.Text = _amount.ToString();
+        }
+
+        private void Decrease(object sender, EventArgs e)
+        {
+            if(_amount <= 0) { return; }
+
+            _amount--;
+
+            lblAmount.Text = _amount.ToString();
         }
     }
 }
